b51ccbc3ad5e6f092855d628e21b259d7c203182
tarikulislam <tarikulislam4145@gmail.com>
2024-02-29 20:01:14 +0530

    init

--- code/src/App.jsx
+++ code/src/App.jsx
@@ -1,13 +1,44 @@
-import TextEditor from "./textEditor";
+import React, { useState } from 'react';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css'; // Import styles
 
-function App() {  
+// Define the toolbar options
+const toolbarOptions = [
+  ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+  ['blockquote', 'code-block'],
+
+  [{ 'header': 1 }, { 'header': 2 }],               // custom button values
+  [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+  [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
+  [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
+  [{ 'direction': 'rtl' }],                         // text direction
+
+  [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+  [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+
+  [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults
+  [{ 'font': [] }],
+  [{ 'align': [] }],
+
+  ['clean'],                                         // remove formatting button
+  ['link', 'image', 'video']                         // link and image, video
+];
+
+const TextEditor = () => {
+  const [text, setText] = useState('');
+
+  const handleChange = value => {
+    setText(value);
+  };
+
   return (
-    <div>
-     
-     
-      <TextEditor />
-    </div>
+    <ReactQuill 
+      theme="snow"
+      value={text}
+      onChange={handleChange}
+      modules={{ toolbar: toolbarOptions }}
+    />
   );
-}
+};
 
-export default App;
+export default TextEditor;
